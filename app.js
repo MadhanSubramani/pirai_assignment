1949ce3ebdd63166c5d9dda93f3eb035d56d14d9
MadhanSubramani <109197587+MadhanSubramani@users.noreply.github.com>
2025-05-23 10:17:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ server.use('/api',authRoutes);
 server.use('/api',todoRoutes);
 server.use('/test',(req,res) => res.status(200).send('Hello World'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.info('Server is listening on ', port);
